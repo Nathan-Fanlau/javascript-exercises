b8f918ee1c9db8fe4f24eb9848236db701350b5e
Nathan Edmund Fanlau <nathanfanlau@gmail.com>
2024-08-20 19:13:05 +0700

    Done 01-05 Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let finalStr = "";
+    for(let i = str.length - 1; i > -1; i--){
+        finalStr = finalStr.concat(str[i]);
+    }
+    return finalStr;
 };
 
 // Do not edit below this line
